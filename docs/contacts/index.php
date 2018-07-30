ecd178759271f470cc83e24881c8517b2f576474
scandal0us <d.l.lutsak@gmail.com>
2018-07-30 17:00:13 +0300

    menu fixed

---
+++ docs/contacts/index.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Regna Bootstrap Template</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="" name="keywords">
+  <meta content="" name="description">
+
+  <!-- Favicons -->
+  <link href="../img/favicon.png" rel="icon">
+  <link href="../img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Poppins:300,400,500,700" rel="stylesheet">
+
+  <!-- Bootstrap CSS File -->
+  <link href="../lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Libraries CSS Files -->
+  <link href="../lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="../lib/animate/animate.min.css" rel="stylesheet">
+
+  <!-- Main Stylesheet File -->
+  <link href="../css/style.css" rel="stylesheet">
+
+<link rel="stylesheet" href="../fontawesome-free-5.1.1-web/css/all.css">
+</head>
+
+<body>
+
+  <!--==========================
+  Header
+  ============================-->
+  <header id="header">
+    <div class="container">
+
+      <div id="logo" class="pull-left">
+        <a href="../index.html"><img src="../img/logo.png" alt="" title="" /></img></a>
+       
+      </div>
+
+      <nav id="nav-menu-container">
+        <ul class="nav-menu">
+          <li class="menu-active"><a href="../rooms.html">Номери</a></li>
+          <li><a href="../restaurant.html">Ресторан</a></li>
+          <li><a href="../spa.html">SPA</a></li>
+          <li><a href="../conferencehall.html">Конференц зала</a></li>
+          <li><a href="../childrenroom.html">Дитяча кімната</a></li>
+          <li><a href="../gallery.html">Галерея</a></li>
+          <li><a href="../prices.html">Ціни</a></li>
+          <li><a href="index.php">Контакти</a></li>
+        </ul>
+      </nav><!-- #nav-menu-container -->
+    </div>
+  </header><!-- #header -->
+
+
+  <main id="main">
+     <!--==========================
+      Services Section
+    ============================-->
+    <section id="services">
+      <div class="container wow fadeIn">
+        <div class="section-header">
+          <h3 class="section-title">Сервіси</h3>
+         
+        </div>
+        <div class="row ">
+          <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.2s">
+            <div class="box rounded">
+              <div class="icon"><a href=""><i class="fas fa-parking"></i></a></div>
+              <h4 class="title"><a href="">ПАРКОВКА</a></h4>
+              <p class="description">Готель VILSON має свою власну парковку для <strong>клієнтів</strong></p>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.4s">
+            <div class="box rounded">
+              <div class="icon"><a href=""><i class="fas fa-phone"></i></a></div>
+              <h4 class="title"><a href="">ТЕЛЕФОНИ</a></h4>
+              <p class="description">Ми будемо раді відповісти на ваші питання або
+                 дати вам вказівки по телефону<br>
+               <strong> <phone>+38 095 000 08 90</phone><br>
+                <phone>+38 098 000 08 90</phone>
+              </strong>
+              </p>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-12 wow fadeInUp" data-wow-delay="0.6s">
+            <div class="box rounded">
+              <div class="icon"><a href=""><i class="fas fa-at"></i></a></div>
+              <h4 class="title"><a href="">E-MAIL</a></h4>
+              <p class="description">Якщо ви хочете задати питання, просто напишіть нам по електронній пошті<br>
+                <br>
+                <email><strong>info@vilson.com.ua</strong></email>
+              </p>
+            </div>
+          </div>
+
+      </div>
+    </section><!-- #services -->
+
+   
+    <section id="maps"><!-- #map-->
+      <div class="container wow fadeIn">
+          <div class="section-header">
+            <br>
+            <br>
+              <h3 class="section-title">Ми знаходимося тут</h3>
+             
+            </div>
+        <div class="row">
+            <div class="col-lg-12 col-md-12 wow fadeInUp" data-wow-delay="0.2s">
+              <div class="map">
+            <iframe  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10604.695878978382!2d24.403899371945904!3d48.35718065814222!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x953684a88822698a!2sVilson+hotel!5e0!3m2!1suk!2sua!4v1531905264807"    style="border:1px solid black; width: 100%;" allowfullscreen>
+            </iframe>
+          </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+  <section id="services"> <!-- #contacForm-->
+      <div class="container wow fadeIn">
+        <div class="section-header">
+         <center>
+          <div class="col-md-6 ">
+              <h1 class="section-title">КОНТАКТНА ФОРМА</h1>
+              <div class="alert alert-primary" role="alert">
+  Поля з зiрочкою ОБОВ'ЯЗКОВІ для заповнення *
+</div>
+    <form action="contactform.php" method="post">
+        <input type="text" name="name" placeholder="Ваше Ім'я *" required>
+        <input type="text" name="surname" placeholder="Ваше Прізвище ">
+        <input type="text" name="mail" placeholder="Ваша електронна адреса " >
+        <input type="text" name="phone" placeholder="Ваш телефон *" required>
+        <input type="text" name="subject" placeholder="Тема">
+        <textarea name="message" placeholder="Повідомлення... *" required></textarea>
+        <button type="submit" class="btn btn-success" name="submit">Відправити</button>
+      </form>
+        </div>
+       </center>
+          </div>
+      </div>
+    </section>
+    
+     </main>
+
+  <!--==========================
+    Footer
+  ============================-->
+  <footer id="footer">
+    <div class="footer-top">
+      <div class="container">
+
+      </div>
+    </div>
+
+    
+  </footer><!-- #footer -->
+
+  <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+
+  <!-- JavaScript Libraries -->
+  <script src="../lib/jquery/jquery.min.js"></script>
+  <script src="../lib/jquery/jquery-migrate.min.js"></script>
+  <script src="../lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="../lib/easing/easing.min.js"></script>
+  <script src="../lib/wow/wow.min.js"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD8HeI8o-c1NppZA-92oYlXakhDPYR7XMY"></script>
+
+  <script src="../lib/waypoints/waypoints.min.js"></script>
+  <script src="../lib/counterup/counterup.min.js"></script>
+  <script src="../lib/superfish/hoverIntent.js"></script>
+  <script src="../lib/superfish/superfish.min.js"></script>
+
+  <!-- Contact Form JavaScript File -->
+  <script src="../contactform/contactform.js"></script>
+
+  <!-- Template Main Javascript File -->
+  <script src="../js/main.js"></script>
+
+</body>
+</html>
